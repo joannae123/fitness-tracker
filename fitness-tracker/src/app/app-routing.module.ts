dd23e0b9241c389ce8238466cb0406b1d963059a
Joanna Emirsajlow <joannae123@gmail.com>
2022-10-06 12:12:34 +0200

    components and routing

--- fitness-tracker/src/app/app-routing.module.ts
+++ fitness-tracker/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './training/training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WelcomeComponent},
+  {path: '', component: SignupComponent},
+  {path: '', component: LoginComponent},
+  {path: '', component: TrainingComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ fitness-tracker/src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
+@NgModule({
+    imports: [
+      BrowserAnimationsModule,
+      MatButtonModule
+    ]
+  })
+
+export class MaterialModule { }
