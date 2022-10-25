ff984781cd42c2371f3ebdca0e16f35584a281a6
Joanna Emirsajlow <joannae123@gmail.com>
2022-10-25 18:05:18 +0200

    signup template

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -3,19 +3,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
     imports: [
       BrowserAnimationsModule,
       MatButtonModule,
       MatInputModule,
-      MatFormFieldModule
+      MatFormFieldModule,
+      MatDatepickerModule,
+      MatNativeDateModule,
+      MatCheckboxModule
     ],
     exports: [
       BrowserAnimationsModule,
       MatButtonModule,
       MatInputModule,
-      MatFormFieldModule
+      MatFormFieldModule,
+      MatDatepickerModule,
+      MatNativeDateModule,
+      MatCheckboxModule
     ]
   })
 
