231cf43ce616122390377d81d0a19de5348e153a
Joanna Emirsajlow <joannae123@gmail.com>
2022-10-27 18:37:10 +0200

    current training

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -13,6 +13,8 @@ import {MatListModule} from '@angular/material/list';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatCardModule} from '@angular/material/card';
 import {MatSelectModule} from '@angular/material/select';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
     imports: [
@@ -29,7 +31,9 @@ import {MatSelectModule} from '@angular/material/select';
       MatListModule,
       MatTabsModule,
       MatCardModule,
-      MatSelectModule
+      MatSelectModule,
+      MatProgressSpinnerModule,
+      MatDialogModule
     ],
     exports: [
       BrowserAnimationsModule,
@@ -45,7 +49,9 @@ import {MatSelectModule} from '@angular/material/select';
       MatListModule,
       MatTabsModule,
       MatCardModule,
-      MatSelectModule
+      MatSelectModule,
+      MatProgressSpinnerModule,
+      MatDialogModule
     ]
   })
 
